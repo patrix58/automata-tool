0f20244f830b2d8a83f8fdfde8349eeedce7764e
Kovacs Patrik <kvcspatrik@gmail.com>
2018-10-12 15:28:05 +0300

    [dev] setup py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='automata',
+    version='0.1',
+    description='This package provides a tool to work with automatas.',
+    author='Kovacs Patrik',
+    packages=['automata'],
+    zip_safe=False
+)
